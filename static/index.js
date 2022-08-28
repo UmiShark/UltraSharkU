705d26dccf1165809030be9ac8a13f2bcf75a14a
UmiShark <62315593+UmiShark@users.noreply.github.com>
2022-08-27 21:58:16 -0400

    Update index.js

--- static/index.js
+++ static/index.js
@@ -70,7 +70,7 @@ function ruffle(url) {
 }
 
 //-----------------------------------BALLS-----------------------------------\\
-
+/*
 const btn = document.querySelector(".checkbox");
 const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 
@@ -97,7 +97,7 @@ btn.addEventListener("click", function() {
 	localStorage.setItem("theme", theme);
 });
 
-/*
+
 const colors = ["#47C1FF", "#A8E2F7", "#007AD9", "#2AA4F4", "#0C52BB"];
 
 const numBalls = 50;
