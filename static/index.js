383d509bd1fc1297c94a94029382c609687d33dd
UmiShark <62315593+UmiShark@users.noreply.github.com>
2022-09-08 16:40:50 -0400

    Update index.js

--- static/index.js
+++ static/index.js
@@ -1,7 +1,3 @@
-iframe = true
-
-//-----------------------------------Ultraviolet-----------------------------------\\
-
 const form = document.querySelector('form');
 const input = document.getElementById('target');
 
@@ -23,52 +19,6 @@ function isUrl(val = '') {
 	if (/^http(s?):\/\//.test(val) || val.includes('.') && val.substr(0, 1) !== ' ') return true;
 	return false;
 };
-
-//-----------------------------------GAMES-----------------------------------\\
-
-function x() {
-	buttons.style.display = "none";
-	bigContainer.style.display = "initial";
-	if (iframe) {
-		console.log('true')
-		iframe_frame.style.display = "none";
-		document.getElementById("iframe_frame").src = "";
-	} else {
-		console.log('false')
-		document.getElementById("ruffle").removeChild(document.getElementById("ruffle").firstChild);
-	}
-}
-
-function reload() {
-	document.getElementById('iframe_frame').contentWindow.location.reload();
-}
-
-
-function loadIframe(url) {
-	iframe = true
-	bigContainer.style.display = "none";
-	iframe_frame.style.display = "initial";
-	buttons.style.display = "flex";
-	document.getElementById("iframe_frame").src = url;
-}
-
-function ruffle(url) {
-	iframe = false;
-	bigContainer.style.display = "none";
-	buttons.style.display = "flex";
-	document.getElementById("ruffle").style.display = "initial";
-	const ruffle = window.RufflePlayer.newest()
-	const player = ruffle.createPlayer();
-	player.config = {
-		letterbox: "on",
-		backgroundColor: "#000000",
-	};
-	player.style.width = "100%";
-	player.style.height = "100%";
-	document.getElementById("ruffle").append(player);
-	player.load(`/swf/${url}.swf`);
-}
-
 
 const btn = document.querySelector(".checkbox");
 const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
@@ -95,50 +45,3 @@ btn.addEventListener("click", function() {
 	}
 	localStorage.setItem("theme", theme);
 });
-
-
-//-----------------------------------BALLS-----------------------------------\\
-/*
-const colors = ["#47C1FF", "#A8E2F7", "#007AD9", "#2AA4F4", "#0C52BB"];
-
-const numBalls = 50;
-const balls = [];
-
-for (let i = 0; i < numBalls; i++) {
-	let ball = document.createElement("div");
-	ball.classList.add("aBalls");
-	ball.style.background = colors[Math.floor(Math.random() * colors.length)];
-	ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
-	ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
-	ball.style.transform = `scale(${Math.random()})`;
-	ball.style.width = `${Math.random()}em`;
-	ball.style.height = ball.style.width;
-
-	balls.push(ball);
-	document.body.append(ball);
-}
-
-// Keyframes
-balls.forEach((el, i, ra) => {
-	let to = {
-		x: Math.random() * (i % 2 === 0 ? -11 : 11),
-		y: Math.random() * 12
-	};
-
-	let anim = el.animate(
-		[{
-				transform: "translate(0, 0)"
-			},
-			{
-				transform: `translate(${to.x}rem, ${to.y}rem)`
-			}
-		], {
-			duration: (Math.random() + 1) * 2000, // random duration
-			direction: "alternate",
-			fill: "both",
-			iterations: Infinity,
-			easing: "ease-in-out"
-		}
-	);
-});
-*/
